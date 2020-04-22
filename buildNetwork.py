2338017d6df60c163c868c2452e02ef1da30e6c9
marcantoine <marcantoine.guery@gmail.com>
2020-04-23 00:02:01 +0200

    Added : - metadata of references - degree selecion Changed : - Dimensions links - Improved node and edge export

--- buildNetwork.py
+++ buildNetwork.py
@@ -5,45 +5,77 @@ import re
 import requests
 import json
 
-#CSV file from Zotero, name of JSON file
-assert len(sys.argv)==3
-
-def cleanhtml(raw_html, cleanr):
+def clean_html(raw_html, cleanr):
   cleantext = re.sub(cleanr, '', raw_html)
   return cleantext
+
+def get_article(title, year):
+    articleSearchUrl='https://app.dimensions.ai/discover/publication/results.json?search_text="%s"&search_type=kws&search_field=text_search&and_facet_year=%i'%(title.replace(" ", "%20"), year)
+    articleSearchResponse = requests.get(articleSearchUrl)
+    article=articleSearchResponse.json()["docs"][0]
+    return article
+
+
+def get_references(dimensionsId):
+    referenceSearchResponse = requests.get(
+    'https://app.dimensions.ai/details/sources/publication/%s/related_citations.json'%dimensionsId)
+    referenceSearchResponse=referenceSearchResponse.json()
+    totalReferences=referenceSearchResponse["count"] if "count" in referenceSearchResponse else 0
+    articleReferences=referenceSearchResponse["docs"] if "docs" in referenceSearchResponse else []
+
+    print("Retrieving "+ str(totalReferences) + " references...")
+    #5 references at a time, I could not find another way
+    while len(articleReferences) < totalReferences:
+        try:
+            cursor=referenceSearchResponse["navigation"]["more.json"].split("?")[-1]
+        except:
+            print("Cannot go further than " + str(len(articleReferences)))
+            break
+        referenceSearchResponse = requests.get(
+        'https://app.dimensions.ai/details/sources/publication/%s/related_citations.json?%s'%(dimensionsId, cursor))
+        referenceSearchResponse=referenceSearchResponse.json()
+        try:
+            assert len(referenceSearchResponse["docs"]) > 0
+        except:
+            print("Empty references, stopped searching at " + str(len(articleReferences)))
+            break
+        articleReferences.extend(referenceSearchResponse["docs"])
+    return articleReferences
+
+def fill_references(references, article, articleReferences, degree):
+    if degree > 0:
+        degree-=1
+        articleReferences=get_references(article["id"])
+        nRef=0
+        for reference in articleReferences:
+            nRef+=1
+            if degree > 0:
+                print(str(nRef)+"/"+str(len(articleReferences))+"...")
+            if reference["id"] not in references or (degree==1 and references[reference["id"]]["references"]==[]) or degree > 1:
+                fill_references(references, reference, [], degree)
+            else:
+                print("Skipping " + reference["id"] + " at leaf+" + str(degree))
+    references[article["id"]]={"article":article, "references":articleReferences}
+    return references
+
+#CSV file from Zotero, name of JSON file
+assert len(sys.argv)==3
 
 cleanr = re.compile('<.*?>')
 
 with open(sys.argv[1], "r") as f:
     rawArticles=pd.read_csv(f)
 
-references={article["Key"]:{"article":{}, "references":[]} for index, article in rawArticles.iterrows()}
+references={}
 #TO DO : parallelize cause it takes forever
-for index, article in rawArticles.iterrows():
-    print("Processing " + article["Key"] + " article...")
-    cleanedTitle=cleanhtml(article["Title"], cleanr)
-    articleSearchUrl="https://app.dimensions.ai/discover/publication/results.json?search_text=%s&search_type=kws&search_field=text_search"%(cleanedTitle.replace(" ", "%20"))
-    articleSearchResponse = requests.get(
-    articleSearchUrl)
-    identifier=articleSearchResponse.json()["docs"][0]["id"]
-    referenceSearchResponse = requests.get(
-    'https://app.dimensions.ai/old/details/publication/%s/related_citations.json'%identifier)
-    referenceSearchResponse=referenceSearchResponse.json()
-    totalReferences=referenceSearchResponse["count"]
-    articleReferences=referenceSearchResponse["docs"]
-    cursor=referenceSearchResponse["navigation"]["more.json"].split("?")[-1]
+for index, rawArticle in rawArticles.iterrows():
+    print("Processing " + rawArticle["Key"] + " article...")
+    cleanedTitle=clean_html(rawArticle["Title"], cleanr)
+    year=int(rawArticle["Publication Year"])
 
-    print("Retrieving "+ str(totalReferences) + " references...")
-    #5 references at a time, I could not find another way
-    while len(articleReferences) < totalReferences:
-        referenceSearchResponse = requests.get(
-        'https://app.dimensions.ai/old/details/publication/%s/related_citations.json?%s'%(identifier, cursor))
-        referenceSearchResponse=referenceSearchResponse.json()
-        articleReferences.extend(referenceSearchResponse["docs"])
-        cursor=referenceSearchResponse["navigation"]["more.json"].split("?")[-1]
-    
-    references[article["Key"]]["article"]=articleSearchResponse.json()["docs"][0]
-    references[article["Key"]]["references"]=articleReferences
+    article=get_article(cleanedTitle, year)
+
+    references=fill_references(references, article, [], 2)
 
 with open(sys.argv[2], 'w', encoding='utf-8') as f:
     json.dump(references, f, ensure_ascii=False, indent=4)
--- makeNetworkFiles.py
+++ makeNetworkFiles.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3.6
+import re
 import sys
 import json
+import csv
+
+def clean_text(raw_text, cleanr):
+    raw_text=re.sub(r'\s+',' ', raw_text).strip()
+    cleantext = re.sub(cleanr, '', raw_text)
+    return cleantext
 
 #JSON file from the builder of this repo, name of node file, name of edge file
 assert len(sys.argv)==4
@@ -10,37 +17,30 @@ with open(sys.argv[1], "r") as f:
 
 ext=".tsv"
 sep="\t"
+quoteStatus=csv.QUOTE_NONE
 
-nodes="\n".join(sep.join([
-    network[identifier]["article"]["title"],
-    network[identifier]["article"]["author_list"].split(",")[0],
-    network[identifier]["article"]["journal_title"],
-    network[identifier]["article"]["html_escaped_title"],
-    network[identifier]["article"]["publisher_source"],
-    network[identifier]["article"]["publisher"],
-    network[identifier]["article"]["pub_class"],
-    str(network[identifier]["article"]["aff_country_count"]),
-    str(network[identifier]["article"]["aff_org_count"]),
-    network[identifier]["article"]["created_in_dimensions"],
-    network[identifier]["article"]["doi"],
-    network[identifier]["article"]["id"],
-    network[identifier]["article"]["issue"] if "issue" in network[identifier]["article"] else "",
-    network[identifier]["article"]["pages"] if "pages" in network[identifier]["article"] else "",
-    network[identifier]["article"]["pmid"] if "pmid" in network[identifier]["article"] else "",
-    network[identifier]["article"]["pub_date"],
-    str(network[identifier]["article"]["pub_year"]),
-    network[identifier]["article"]["volume"] if "volume" in network[identifier]["article"] else "",
-    str(network[identifier]["article"]["times_cited"]),
-    str(network[identifier]["article"]["score"]),
-    str(network[identifier]["article"]["altmetric_id"]),
-    network[identifier]["article"]["source_title"],
-    network[identifier]["article"]["pub_class_id"]]) for identifier in network)
+with open(sys.argv[2]+ext, 'w') as nodefile:
+    fieldnames = [
+        "title", "author_list", "journal_title",
+        "html_escaped_title", "publisher_source", "pub_class",
+        "doi", "id", "pub_year", 
+        "times_cited", "altmetric", "source_title"]
+    writer = csv.DictWriter(nodefile, fieldnames=fieldnames, extrasaction="ignore", delimiter=sep, quoting=quoteStatus)
 
-edges="\n".join("\n".join(sep.join([network[identifier]["article"]["id"], reference["id"], "1"]) for reference in network[identifier]["references"]) for identifier in network)
+    writer.writeheader()
+    cleanr = re.compile('(<.*?>|\")')
+    for identifier in network:
+        lineToWrite={key:clean_text(str(item), cleanr) for key, item in network[identifier]["article"].items() if key in fieldnames}
+        try:
+            writer.writerow(lineToWrite)
+        except:
+            print(lineToWrite)
+            exit(1)
 
-with open(sys.argv[2]+ext, "w") as f:
-    f.write(nodes)
-
-with open(sys.argv[3]+ext, "w") as f:
-    f.write(sep.join(["Source", "Target", "Weigth"])+"\n")
-    f.write(edges)
+with open(sys.argv[3]+ext, "w") as edgefile:
+    writer = csv.writer(edgefile, delimiter=sep,quoting=quoteStatus)
+    writer.writerow(["Source", "Target", "Weigth"])
+    for identifier in network:
+        if network[identifier]["references"]!=[]:
+            for reference in network[identifier]["references"]:
+                writer.writerow([network[identifier]["article"]["id"], reference["id"], "1"])
